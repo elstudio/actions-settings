5788789fcd2d2c12f22f9c381e9d4bab728df431
Eric Johnson <elstudio@users.noreply.github.com>
2019-07-07 17:04:27 -0400

    Make workflow run only once.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,3 +11,10 @@ fi
 
 # Launch our probot app
 probot receive -e $GITHUB_EVENT_NAME -p $GITHUB_EVENT_PATH /app/node_modules/probot-settings/index.js
+
+# Try to remove the workflow file so we execute only when the repository is created
+cd $GITHUB_WORKSPACE
+rm .github/main.workflow
+git add .github/main.workflow
+git commit -m "Remove workflow file so it runs only once."
+git push
